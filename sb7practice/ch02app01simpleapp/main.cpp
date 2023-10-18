be6465ac16e442009d14d85151d98dd37b3947b2
RolindaW <56414667+RolindaW@users.noreply.github.com>
2023-10-19 01:19:48 +0200

    app01-simpleapp: calculate buffer clear color dynamically

--- sb7practice/ch02app01simpleapp/main.cpp
+++ sb7practice/ch02app01simpleapp/main.cpp
@@ -1,6 +1,9 @@
 // Include the "sb7.h" header file
 #include "sb7.h"
 
+// Include "math.h" library to get access to sin() and cos() functions
+#include "math.h"
+
 // Derive my_application from sb7::application
 class my_application : public sb7::application
 {
@@ -8,9 +11,22 @@ public:
 	// Our rendering function
 	void render(double currentTime)
 	{
-		// Simply clear the window with red
-		static const GLfloat red[] = { 1.0f, 0.0f, 0.0f, 1.0f };
-		glClearBufferfv(GL_COLOR, 0, red);
+		const bool fixed_color = false;
+		if (fixed_color)
+		{
+			// Simply clear the window with red
+			static const GLfloat red[] = { 1.0f, 0.0f, 0.0f, 1.0f };
+			glClearBufferfv(GL_COLOR, 0, red);
+		}
+		else
+		{
+			// Calculate new color based on elapsed time since the application is started
+			const GLfloat color[] = { (float)sin(currentTime) * 0.5f + 0.5f,
+			(float)cos(currentTime) * 0.5f + 0.5f,
+			0.0f,
+			1.0f };
+			glClearBufferfv(GL_COLOR, 0, color);
+		}
 	}
 };
 

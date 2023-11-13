3500e14642947e680b67a29e97c9df7e3e038a50
RolindaW <56414667+RolindaW@users.noreply.github.com>
2023-11-13 17:38:19 +0100

    ch05-app01-bufferinit: fixes and extra stuff

--- sb7practice/ch05app01bufferinitialization/main.cpp
+++ sb7practice/ch05app01bufferinitialization/main.cpp
@@ -7,42 +7,65 @@ class my_application : public sb7::application
 public:
 	void startup()
 	{
+		buffer = create_buffers();
+	}
+
+	void render(double currentTime)
+	{
+		// Put into a buffer is a constant value (replicate the data across the range of the buffer’s data store)
+		//const float new_data = sin(currentTime);
+		//glClearNamedBufferSubData(buffer, GL_R32F, 0, 4 * 4, GL_RED, GL_FLOAT, &new_data);
+		const float new_data[] = { sin(currentTime), cos(currentTime) };
+		glClearNamedBufferSubData(buffer, GL_RG32F, 0, 4 * 4, GL_RG, GL_FLOAT, new_data);
+
+		float buffer_data[12];
+		glGetNamedBufferSubData(buffer, 0, 4 * 12, &buffer_data[0]);
+	}
+
+	void shutdown()
+	{
+		glDeleteBuffers(1, &buffer);
+	}
+
+private:
+	// Buffers creation
+	GLuint create_buffers(void)
+	{
 		// Create a buffer object (and corresponding name)
 		GLuint buffer;
 		glCreateBuffers(1, &buffer);
-
 		/*GLuint buffers[2];
 		glCreateBuffers(2, &buffers[0]);*/  // Create multiple buffers one time
 
 		// Specify the data store parameters for the buffer
-		glNamedBufferStorage(buffer,	// The target the buffer object is bound to
-							 1024 * 1024,	// 1 MiB of space - in bytes
-							 NULL,	// No initial data
-							 GL_DYNAMIC_STORAGE_BIT & GL_MAP_WRITE_BIT);	// Allow map for writting
+		glNamedBufferStorage(buffer,	// Name of the buffer
+			1024 * 1024,	// 1 MiB of space - in bytes
+			NULL,	// No initial data
+			GL_DYNAMIC_STORAGE_BIT | GL_MAP_WRITE_BIT);	// Allow map for writting
 		/*glBufferStorage(GL_ARRAY_BUFFER,
 						1024 * 1024,
 						NULL,
 						GL_DYNAMIC_STORAGE_BIT & GL_MAP_WRITE_BIT);*/  // Pre-binding required
 
-		// Bind a named buffer object to a target
+						// Bind a named buffer to a target
 		glBindBuffer(GL_ARRAY_BUFFER, buffer);  // Store vertex data
 
-		// This is the data that we will place into the buffer object
+		// This is the data that we will place into the buffer
 		static const float data[] = {
 			 0.25, -0.25, 0.5, 1.0,
 			-0.25, -0.25, 0.5, 1.0,
 			 0.25,  0.25, 0.5, 1.0
 		};
-		
+
 		// Put the data into the buffer
 		bool map = false;
-		if (map)
+		if (!map)
 		{
 			// Copy data
 			glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(data), data);
 			//glNamedBufferSubData(buffer, 0, sizeof(data), data);
 		}
-		else  // Map buffer object and then copy the data there yourself.
+		else  // Map buffer and then copy the data there yourself.
 		{
 			// Get a pointer to the buffer's data store
 			void* ptr = glMapNamedBuffer(buffer, GL_WRITE_ONLY);
@@ -59,7 +82,12 @@ public:
 			glUnmapNamedBuffer(buffer);
 			//glUnmapBuffer(GL_ARRAY_BUFFER);
 		}
+
+		return buffer;
 	}
+
+private:
+	GLuint buffer;
 };
 
 // Our one and only instance of DECLARE_MAIN

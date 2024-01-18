6bd901d261b8cc2f959f69a7c4c81663df0ee534
RolindaW <56414667+RolindaW@users.noreply.github.com>
2024-01-18 15:33:03 +0100

    ch05-app13-per-pixel-linked-list: atomic counter and image framebuffer

--- sb7practice/ch05app13perpixellinkedlist/main.cpp
+++ sb7practice/ch05app13perpixellinkedlist/main.cpp
@@ -12,6 +12,8 @@ public:
 		InitializePrograms();
 		InitializeCamera();
 		InitializeObject();
+		InitializeAtomicCounter();
+		InitializeFramebuffer();
 	}
 
 	void render(double currentTime)
@@ -20,7 +22,37 @@ public:
 		static const GLfloat color[] = { 0.0f, 0.0f, 0.0f, 1.0f };
 		glClearBufferfv(GL_COLOR, 0, color);
 
+		// *** Fill linked list
+
 		glUseProgram(fillingProgram);
+
+		ResetAtomicCounter();
+		glMemoryBarrier(GL_ATOMIC_COUNTER_BARRIER_BIT);
+		glBindBufferBase(GL_ATOMIC_COUNTER_BUFFER, 0, acbo);
+
+		ResetFramebuffer();
+		glMemoryBarrier(GL_SHADER_IMAGE_ACCESS_BARRIER_BIT);
+		glBindImageTexture(0, texture, 0, GL_FALSE, 0, GL_READ_WRITE, GL_R32UI);
+
+		// TODO: Reset linked list (shader storage block)
+
+		glUniformMatrix4fv(0, 1, GL_FALSE, viewMatrix * modelWorldMatrix);
+		glUniformMatrix4fv(1, 1, GL_FALSE, projectionMatrix);
+
+		object.render();
+
+		// Read the value of the atomic counter and save the highest value (just because curious) - in this case it depends on object orientation
+		GLuint atomicCounter = CheckAtomicCounter();
+		atomicCounterMaxValue = atomicCounter > atomicCounterMaxValue ? atomicCounter : atomicCounterMaxValue;
+
+		// TODO: Barriers: atomic counter, texture image and shader storage block
+		glMemoryBarrier(GL_ATOMIC_COUNTER_BARRIER_BIT);
+		glMemoryBarrier(GL_SHADER_IMAGE_ACCESS_BARRIER_BIT);
+
+		// TODO: *** Traverse linked list
+		glUseProgram(traversingProgram);
+
+		glBindImageTexture(0, texture, 0, GL_FALSE, 0, GL_READ_ONLY, GL_R32UI);
 
 		glUniformMatrix4fv(0, 1, GL_FALSE, viewMatrix * modelWorldMatrix);
 		glUniformMatrix4fv(1, 1, GL_FALSE, projectionMatrix);
@@ -32,6 +64,8 @@ public:
 	{
 		DestroyPrograms();
 		DestroyObject();
+		DestroyAtomicCounter();
+		DestroyFramebuffer();
 	}
 
 public:
@@ -87,11 +121,25 @@ private:
 		const GLchar* fillingFragmentShaderSource[] = {
 			"#version 450 core													\n"
 			"																	\n"
-			"out vec4 color;													\n"
+			"// Atomic counter for filled size									\n"
+			"layout (binding = 0, offset = 0) uniform atomic_uint fill_counter;	\n"
+			"																	\n"
+			"// 2D image to store head pointers									\n"
+			"layout (binding = 0, r32ui) uniform uimage2D head_pointer;			\n"
+			"																	\n"
+			"//out vec4 color;													\n"
 			"																	\n"
 			"void main(void)													\n"
 			"{																	\n"
-			"	color = vec4(0.0, 1.0, 0.0, 1.0);								\n"
+			"	ivec2 P = ivec2(gl_FragCoord.xy);								\n"
+			"																	\n"
+			"	uint index = atomicCounterIncrement(fill_counter);				\n"
+			"	memoryBarrierAtomicCounter();									\n"
+			"																	\n"
+			"	uint old_head = imageAtomicExchange(head_pointer, P, index);	\n"
+			"	memoryBarrierImage();											\n"
+			"																	\n"
+			"	//color = vec4(0.0, 1.0, 0.0, 1.0);								\n"
 			"}																	\n"
 		};
 
@@ -108,11 +156,25 @@ private:
 		const GLchar* traversingFragmentShaderSource[] = {
 			"#version 450 core													\n"
 			"																	\n"
+			"// 2D image storing head pointers									\n"
+			"layout (binding = 0, r32ui) readonly uniform uimage2D head_pointer;\n"
+			"																	\n"
 			"out vec4 color;													\n"
 			"																	\n"
 			"void main(void)													\n"
 			"{																	\n"
-			"	color = vec4(0.0, 1.0, 0.0, 1.0);								\n"
+			"	ivec2 P = ivec2(gl_FragCoord.xy);								\n"
+			"																	\n"
+			"	uint index = imageLoad(head_pointer, P).x;						\n"
+			"																	\n"
+			"	if (index == 0xFFFFFFFF)										\n"
+			"	{																\n"
+			"		color = vec4(0.0, 0.0, 0.0, 1.0);							\n"
+			"	}																\n"
+			"	else															\n"
+			"	{																\n"
+			"		color = vec4(1.0, 1.0, 1.0, 1.0);							\n"
+			"	}																\n"
 			"}																	\n"
 		};
 
@@ -129,6 +191,12 @@ private:
 		glDeleteShader(vertexShader);
 		glDeleteShader(fillingFragmentShader);
 		glDeleteShader(traversingFragmentShader);
+	}
+
+	void DestroyPrograms()
+	{
+		glDeleteProgram(fillingProgram);
+		glDeleteProgram(traversingProgram);
 	}
 
 	void InitializeCamera()
@@ -160,29 +228,86 @@ private:
 		modelWorldMatrix = vmath::rotate(0.0f, ccw * objectRotationYStep, 0.0f) * modelWorldMatrix;
 	}
 
-	void DestroyPrograms()
-	{
-		glDeleteProgram(fillingProgram);
-		glDeleteProgram(traversingProgram);
-	}
-
 	void DestroyObject()
 	{
 		object.free();
 	}
+
+	void InitializeAtomicCounter()
+	{
+		const GLuint data = (GLuint)3;  // Only to verify atomic counter is reset first rendering cycle
+		glCreateBuffers(1, &acbo);
+		glNamedBufferStorage(acbo, sizeof(GLuint), &data, GL_MAP_WRITE_BIT | GL_MAP_READ_BIT);
+	}
+
+	void ResetAtomicCounter()
+	{
+		GLuint* acboData = (GLuint*)glMapNamedBufferRange(acbo,
+															0, sizeof(GLuint),
+															GL_MAP_WRITE_BIT);
+		*acboData = (GLuint)0;
+		glUnmapNamedBuffer(acbo);
+	}
+
+	GLuint CheckAtomicCounter()
+	{
+		GLuint* acboData = (GLuint*)glMapNamedBufferRange(acbo,
+															0, sizeof(GLuint),
+															GL_MAP_READ_BIT);
+
+		glUnmapNamedBuffer(acbo);
+
+		return *acboData;
+	}
+
+	void DestroyAtomicCounter()
+	{
+		glDeleteBuffers(1, &acbo);
+	}
+
+	void InitializeFramebuffer()
+	{
+		// Create a new 2D texture object
+		glCreateTextures(GL_TEXTURE_2D, 1, &texture);
+
+		// Specify the amount of storage we want to use for the texture
+		glTextureStorage2D(texture,
+			1,
+			GL_R32UI,
+			info.windowWidth, info.windowHeight);
+	}
+
+	void ResetFramebuffer()
+	{
+		const GLuint data[] = { 0xFFFFFFFF };  // Security value
+		glClearTexImage(texture, 0, GL_RED_INTEGER, GL_UNSIGNED_INT, data);
+
+		glMemoryBarrier(GL_SHADER_IMAGE_ACCESS_BARRIER_BIT);
+	}
+
+	void DestroyFramebuffer()
+	{
+		glDeleteTextures(1, &texture);
+	}
+
 private:
 	GLuint fillingProgram;
 	GLuint traversingProgram;
 
 	sb7::object object;
 	vmath::mat4 modelWorldMatrix;
-	const float objectRotationYStep = 15.0f;
+	const float objectRotationYStep = 1.0f;
 
 	vmath::mat4 viewMatrix;
 	vmath::mat4 projectionMatrix;
+
+	GLuint acbo;
+	GLuint atomicCounterMaxValue = 0;
+
+	GLuint texture;
 };
 
-// TODO: Update fragment shader code and implement correspoding GL code
+// TODO: Implement shader storage block list in ither GL code and shader code
 
 // Our one and only instance of DECLARE_MAIN
 DECLARE_MAIN(my_application);

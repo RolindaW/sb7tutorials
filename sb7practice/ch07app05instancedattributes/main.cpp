19ae82b1105f2458d15517738217a1dd72645c1a
RolindaW <56414667+RolindaW@users.noreply.github.com>
2024-02-26 16:32:09 +0100

    ch07-drawing-commands: instanced attributes fix project and documentation

--- sb7practice/ch07app05instancedattributes/main.cpp
+++ sb7practice/ch07app05instancedattributes/main.cpp
@@ -10,7 +10,7 @@ public:
 	{
 		InitializeCamera();
 		InitializeProgram();
-		InitializeObject();
+		InitializeObject(0);
 	}
 
 	void render(double currentTime)
@@ -46,6 +46,7 @@ public:
 	}
 
 private:
+
 #pragma region Camera
 
 	void InitializeCamera()
@@ -93,7 +94,7 @@ private:
 			"{																				\n"
 			"	vec2 instance_position = vec2(position + instance_position_offset);			\n"
 			"	gl_Position = vp_matrix * vec4(instance_position, 0.0, 1.0);				\n"
-			"	vs_color = instance_color;													\n"
+			"	vs_color = instance_color + vec4(0.1, 0.0, 0.1, 1.0);						\n"
 			"}																				\n"
 		};
 
@@ -135,7 +136,31 @@ private:
 
 #pragma region Object
 
-	void InitializeObject()
+	void InitializeObject(int mode)
+	{
+		switch (mode)
+		{
+		case 0:
+			InitializeObject_01MultipleBindingPoints();
+			break;
+		case 1:
+			InitializeObject_02SharedBindingPoint();
+			break;
+		case 2:
+			InitializeObject_03InterleavedMultipleBindingPoints();
+			break;
+		case 3:
+			InitializeObject_04InterleavedSharedBindingPoint();
+			break;
+		case 4:
+			InitializeObject_05InterleavedMultipleMixedBindingPoints();
+			break;
+		default:
+			break;
+		}
+	}
+
+	void InitializeObject_01MultipleBindingPoints()
 	{
 		// Create and bind VAO (Vertex Array Object)
 		glCreateVertexArrays(1, &vao);
@@ -182,19 +207,212 @@ private:
 
 		// Instance position offsets
 		glVertexArrayAttribFormat(vao, 1, 2, GL_FLOAT, GL_FALSE, 0);
-		glVertexArrayAttribBinding(vao, 1, 1);
-		glVertexArrayVertexBuffer(vao, 1, vbo, sizeof(vertex_positions), sizeof(GLfloat) * 2);
-		glVertexAttribDivisor(1, 1);  // Saved into active VAO
-		//glVertexArrayBindingDivisor();
-		//glVertexBindingDivisor();
+		glVertexArrayAttribBinding(vao, 1, 6);
+		glVertexArrayVertexBuffer(vao, 6, vbo, sizeof(vertex_positions), sizeof(GLfloat) * 2);
+		glVertexAttribDivisor(6, 1);  // Saved into active VAO. Warning! Specified by buffer binding point index, not by vertex attribute index
+		//glVertexBindingDivisor(6, 1);
+		//glVertexArrayBindingDivisor(vao, 6, 1);
 		glEnableVertexArrayAttrib(vao, 1);
 
 		// Instance colors
 		glVertexArrayAttribFormat(vao, 2, 4, GL_UNSIGNED_BYTE, GL_TRUE, 0);
-		glVertexArrayAttribBinding(vao, 2, 2);
-		glVertexArrayVertexBuffer(vao, 2, vbo, sizeof(vertex_positions) + sizeof(instance_position_offsets), sizeof(GLubyte) * 4);
-		glVertexAttribDivisor(2, 1);
+		glVertexArrayAttribBinding(vao, 2, 7);
+		glVertexArrayVertexBuffer(vao, 7, vbo, sizeof(vertex_positions) + sizeof(instance_position_offsets), sizeof(GLubyte) * 4);
+		glVertexAttribDivisor(7, 1);
 		glEnableVertexArrayAttrib(vao, 2);
+	}
+
+	// Error! This approach is not factible as each attribute requires a different stride
+	void InitializeObject_02SharedBindingPoint()
+	{
+		// Create and bind VAO (Vertex Array Object)
+		glCreateVertexArrays(1, &vao);
+		glBindVertexArray(vao);
+
+		// Define (non-instanced and instanced) vertex attributes values
+		const GLfloat vertex_positions[] = {
+			-1.0f, -1.0f,
+			 1.0f, -1.0f,
+			 1.0f,  1.0f,
+			-1.0f,  1.0f
+		};
+
+		const GLfloat instance_position_offsets[] = {
+			-2.0f, -2.0f,
+			 2.0f, -2.0f,
+			 2.0f,  2.0f,
+			-2.0f,  2.0f
+		};
+
+		const GLubyte instance_colors[] = {
+			0xFF, 0x00, 0x00, 0xFF,
+			0x00, 0xFF, 0x00, 0xFF,
+			0x00, 0x00, 0xFF, 0xFF,
+			0xFF, 0xFF, 0x00, 0xFF
+		};
+
+		// Create VBO (Vertex Buffer Object), allocate memory and store data
+		glCreateBuffers(1, &vbo);
+		const unsigned int bufferSize = sizeof(vertex_positions) + sizeof(instance_position_offsets) + sizeof(instance_colors);
+		glNamedBufferStorage(vbo, bufferSize, vertex_positions, GL_DYNAMIC_STORAGE_BIT);
+		unsigned int offset = sizeof(vertex_positions);
+		glNamedBufferSubData(vbo, offset, sizeof(instance_position_offsets), instance_position_offsets);
+		offset += sizeof(instance_position_offsets);
+		glNamedBufferSubData(vbo, offset, sizeof(instance_colors), instance_colors);
+
+		// Setup and enable vertex attributes feeding in VAO
+
+		// Vertex positions
+		glVertexArrayAttribFormat(vao, 0, 2, GL_FLOAT, GL_FALSE, 0);
+		glVertexArrayAttribBinding(vao, 0, 0);
+		glEnableVertexArrayAttrib(vao, 0);
+		
+		// Instance position offsets
+		glVertexArrayAttribFormat(vao, 1, 2, GL_FLOAT, GL_FALSE, sizeof(vertex_positions));
+		glVertexArrayAttribBinding(vao, 1, 0);
+		//glVertexAttribDivisor(1, 1);
+		glEnableVertexArrayAttrib(vao, 1);
+		
+		// Instance colors
+		glVertexArrayAttribFormat(vao, 2, 4, GL_UNSIGNED_BYTE, GL_TRUE, sizeof(instance_position_offsets));
+		glVertexArrayAttribBinding(vao, 2, 0);
+		//glVertexAttribDivisor(2, 1);
+		glEnableVertexArrayAttrib(vao, 2);
+		
+		// Define common buffer binding point
+		//glVertexArrayVertexBuffer(vao, 0, vbo, 0, ???);
+	}
+
+	// When using interleaved memory layout and multiple buffer binding points it is possible to interchangeably use buffer binding point base offset and attribute relative offset (to mapped buffer binding point)
+	void InitializeObject_03InterleavedMultipleBindingPoints()
+	{
+		// Create and bind VAO (Vertex Array Object)
+		glCreateVertexArrays(1, &vao);
+		glBindVertexArray(vao);
+
+		// Define (non-instanced and instanced) vertex attributes values
+		const GLfloat data[] = {
+			-1.0f, -1.0f, -2.0f, -2.0f, 1.0f, 0.0f, 0.0f, 1.0f,
+			 1.0f, -1.0f,  2.0f, -2.0f, 0.0f, 1.0f, 0.0f, 1.0f,
+			 1.0f,  1.0f,  2.0f,  2.0f, 0.0f, 0.0f, 1.0f, 1.0f,
+			-1.0f,  1.0f, -2.0f,  2.0f, 1.0f, 1.0f, 0.0f, 1.0f
+		};
+
+		// Create VBO (Vertex Buffer Object), allocate memory and store data
+		glCreateBuffers(1, &vbo);
+		glNamedBufferStorage(vbo, sizeof(data), data, NULL);
+
+		// Setup and enable vertex attributes feeding in VAO
+
+		unsigned int stride = sizeof(GLfloat) * 8;
+
+		// Vertex positions
+		glVertexArrayAttribFormat(vao, 0, 2, GL_FLOAT, GL_FALSE, 0);
+		glVertexArrayAttribBinding(vao, 0, 0);
+		glVertexArrayVertexBuffer(vao, 0, vbo, 0, stride);
+		glEnableVertexArrayAttrib(vao, 0);
+
+		// Instance position offsets
+		glVertexArrayAttribFormat(vao, 1, 2, GL_FLOAT, GL_FALSE, 0);  //glVertexArrayAttribFormat(vao, 1, 2, GL_FLOAT, GL_FALSE, sizeof(GLfloat) * 2);
+		glVertexArrayAttribBinding(vao, 1, 6);
+		glVertexArrayVertexBuffer(vao, 6, vbo, sizeof(GLfloat) * 2, stride);  //glVertexArrayVertexBuffer(vao, 1, vbo, 0, stride);
+		glVertexAttribDivisor(6, 1);
+		glEnableVertexArrayAttrib(vao, 1);
+
+		// Instance colors
+		glVertexArrayAttribFormat(vao, 2, 4, GL_FLOAT, GL_FALSE, 0);  //glVertexArrayAttribFormat(vao, 2, 4, GL_FLOAT, GL_FALSE, sizeof(GLfloat) * 4);
+		glVertexArrayAttribBinding(vao, 2, 7);
+		glVertexArrayVertexBuffer(vao, 7, vbo, sizeof(GLfloat) * 4, stride);  //glVertexArrayVertexBuffer(vao, 2, vbo, 0, stride);
+		glVertexAttribDivisor(7, 1);
+		glEnableVertexArrayAttrib(vao, 2);
+	}
+
+	// Warning! Using interleaved memory layout and single buffer binding point does not properly work when mixing vertex attributes with instanced vertex attributes; process all data as pure vertex attributes
+	// Error! This approach is not factible as it is not possible to use same buffer binding point for both pure vertex attributes (divisor = 0) and instanced vertex attribute (divisor != 0) at the same time
+	void InitializeObject_04InterleavedSharedBindingPoint()
+	{
+		// Create and bind VAO (Vertex Array Object)
+		glCreateVertexArrays(1, &vao);
+		glBindVertexArray(vao);
+
+		// Define (non-instanced and instanced) vertex attributes values
+		const GLfloat data[] = {
+			-1.0f, -1.0f, -2.0f, -2.0f, 1.0f, 0.0f, 0.0f, 1.0f,
+			 1.0f, -1.0f,  2.0f, -2.0f, 0.0f, 1.0f, 0.0f, 1.0f,
+			 1.0f,  1.0f,  2.0f,  2.0f, 0.0f, 0.0f, 1.0f, 1.0f,
+			-1.0f,  1.0f, -2.0f,  2.0f, 1.0f, 1.0f, 0.0f, 1.0f
+		};
+
+		// Create VBO (Vertex Buffer Object), allocate memory and store data
+		glCreateBuffers(1, &vbo);
+		glNamedBufferStorage(vbo, sizeof(data), data, NULL);
+
+		// Setup and enable vertex attributes feeding in VAO
+
+		// Vertex positions
+		glVertexArrayAttribFormat(vao, 0, 2, GL_FLOAT, GL_FALSE, 0);
+		glVertexArrayAttribBinding(vao, 0, 0);
+		glEnableVertexArrayAttrib(vao, 0);
+
+		// Instance position offsets
+		glVertexArrayAttribFormat(vao, 1, 2, GL_FLOAT, GL_FALSE, sizeof(GLfloat) * 2);
+		glVertexArrayAttribBinding(vao, 1, 0);  // Warning! Calling this method clear divisor configuration for corresponding vertex attribute
+		//glVertexAttribDivisor(1, 1);  // Error! Specifing a divisor by an instanced vertex attribute index that is bound to a buffer binding point makes the vertex attribute data not to be correctly processed
+		glEnableVertexArrayAttrib(vao, 1);
+
+		// Instance colors
+		glVertexArrayAttribFormat(vao, 2, 4, GL_FLOAT, GL_FALSE, sizeof(GLfloat) * 4);
+		glVertexArrayAttribBinding(vao, 2, 0);
+		//glVertexAttribDivisor(2, 1);
+		glEnableVertexArrayAttrib(vao, 2);
+		
+		// Define common buffer binding point
+		unsigned int stride = sizeof(GLfloat) * 8;
+		glVertexArrayVertexBuffer(vao, 0, vbo, 0, stride);
+	}
+
+	void InitializeObject_05InterleavedMultipleMixedBindingPoints()
+	{
+		// Create and bind VAO (Vertex Array Object)
+		glCreateVertexArrays(1, &vao);
+		glBindVertexArray(vao);
+
+		// Define (non-instanced and instanced) vertex attributes values
+		const GLfloat data[] = {
+			-1.0f, -1.0f, -2.0f, -2.0f, 1.0f, 0.0f, 0.0f, 1.0f,
+			 1.0f, -1.0f,  2.0f, -2.0f, 0.0f, 1.0f, 0.0f, 1.0f,
+			 1.0f,  1.0f,  2.0f,  2.0f, 0.0f, 0.0f, 1.0f, 1.0f,
+			-1.0f,  1.0f, -2.0f,  2.0f, 1.0f, 1.0f, 0.0f, 1.0f
+		};
+
+		// Create VBO (Vertex Buffer Object), allocate memory and store data
+		glCreateBuffers(1, &vbo);
+		glNamedBufferStorage(vbo, sizeof(data), data, NULL);
+
+		// Setup and enable vertex attributes feeding in VAO
+
+		// Vertex positions
+		glVertexArrayAttribFormat(vao, 0, 2, GL_FLOAT, GL_FALSE, 0);
+		glVertexArrayAttribBinding(vao, 0, 0);
+		glVertexArrayVertexBuffer(vao, 0, vbo, 0, sizeof(GLfloat) * 8);
+		glEnableVertexArrayAttrib(vao, 0);
+
+		// Instance position offsets
+		glVertexArrayAttribFormat(vao, 1, 2, GL_FLOAT, GL_FALSE, sizeof(GLfloat) * 2);
+		glVertexArrayAttribBinding(vao, 1, 7);  // Warning! Calling this method clear divisor configuration for corresponding vertex attribute
+		//glVertexAttribDivisor(1, 1);  // Error! Specifing a divisor by an instanced vertex attribute index that is bound to a buffer binding point makes the vertex attribute data not to be correctly processed
+		glEnableVertexArrayAttrib(vao, 1);
+
+		// Instance colors
+		glVertexArrayAttribFormat(vao, 2, 4, GL_FLOAT, GL_FALSE, sizeof(GLfloat) * 4);
+		glVertexArrayAttribBinding(vao, 2, 7);
+		//glVertexAttribDivisor(2, 1);
+		glEnableVertexArrayAttrib(vao, 2);
+
+		// Define common buffer binding point for instanced vertex attributes
+		// Note: In this case is possible to use a common binding point for instanced vertex attributes because they use same divisor value
+		glVertexArrayVertexBuffer(vao, 7, vbo, 0, sizeof(GLfloat) * 8);
+		glVertexAttribDivisor(7, 1);
 	}
 
 #pragma endregion
